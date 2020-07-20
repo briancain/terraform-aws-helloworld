79e42d68b61d049a83333201c1953a54aa141fa8
Brian Cain <bcain@hashicorp.com>
2020-07-20 11:04:38 -0700

    Add two resources

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "null_resource" "hello_world" {
   provisioner "local-exec" {
+    command = "echo hello wuuurrrlllld!!"
+  }
+}
+
+resource "null_resource" "hello_world part 2" {
+  provisioner "local-exec" {
     command = "echo ${var.greetings}!"
   }
 }
